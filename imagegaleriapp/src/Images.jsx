f95972f466b751add91a9d83ac7206f1bf691976
kubranuryavuz <nurkacm@gmail.com>
2024-06-12 10:07:57 +0300

    project completed

---
+++ imagegaleriapp/src/Images.jsx
@@ -0,0 +1,8 @@
+export default[
+"https://cdn.yemek.com/uploads/2020/10/humus-yatay-yilbasi.jpg",
+"https://mezekoy.com/wp-content/uploads/2023/08/acili_ezme.png",
+"https://i0.wp.com/portakalagaci.com/wp-content/uploads/2022/05/semizotu2.jpg?resize=930%2C620&ssl=1",
+"https://www.anitegitim.com/wp-content/uploads/2023/10/Mezeler-Yazisi-One-Cikan-Gorsel.webp",
+"https://static.wixstatic.com/media/6cd36f_5f4959f0d72d4d8a84b5a13842060eca~mv2.jpg/v1/fill/w_1962,h_1002,al_b,q_90,usm_0.66_1.00_0.01,enc_auto/6cd36f_5f4959f0d72d4d8a84b5a13842060eca~mv2.jpg"
+
+]
